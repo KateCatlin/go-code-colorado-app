7877c32dd88d8643d73f0bad397b3a6ba405e170
Kate Catlin <kcatlin2@gmail.com>
2017-04-08 15:36:47 -0600

    Adding new question_flow class

--- app/src/main/java/com/example/katecatlin/diversityapp/ChatActivity.java
+++ app/src/main/java/com/example/katecatlin/diversityapp/ChatActivity.java
@@ -4,6 +4,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 /**
  * Created by katecatlin on 4/8/17.
  */
@@ -14,6 +20,14 @@ public class ChatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+        QuestionFlow questionFlow;
+
+        try {
+            final InputStream inputStream = getAssets().open("question_flow.json");
+            final BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
+            questionFlow = new Gson().fromJson(reader, QuestionFlow.class);
+        } catch (Exception e) {
+        }
     }
 
     private void advanceToStats() {
---
+++ app/src/main/java/com/example/katecatlin/diversityapp/QuestionFlow.java
@@ -0,0 +1,10 @@
+package com.example.katecatlin.diversityapp;
+
+/**
+ * Created by katecatlin on 4/8/17.
+ */
+
+public class QuestionFlow {
+
+
+}
