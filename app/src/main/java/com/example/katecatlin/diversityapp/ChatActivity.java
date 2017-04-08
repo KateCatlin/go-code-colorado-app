46837d58654f293b50a5bbbd1ab115badd92d4b4
Stuart Kent <skent.dev@gmail.com>
2017-04-08 15:16:40 -0600

    Android: add chat library UI and advance hook

--- app/src/main/java/com/example/katecatlin/diversityapp/ChatActivity.java
+++ app/src/main/java/com/example/katecatlin/diversityapp/ChatActivity.java
@@ -1,8 +1,8 @@
 package com.example.katecatlin.diversityapp;
 
-import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by katecatlin on 4/8/17.
@@ -15,4 +15,10 @@ public class ChatActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
     }
+
+    private void advanceToStats() {
+        Intent intent = new Intent(this, StatActivity.class);
+        startActivity(intent);
+    }
+
 }
