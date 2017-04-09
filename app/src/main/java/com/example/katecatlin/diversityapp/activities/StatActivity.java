47db7111890a7a759726c1e70cb391957aa09411
Kate Catlin <kcatlin2@gmail.com>
2017-04-09 13:44:46 -0600

    opening URL

--- app/src/main/java/com/example/katecatlin/diversityapp/activities/StatActivity.java
+++ app/src/main/java/com/example/katecatlin/diversityapp/activities/StatActivity.java
@@ -1,7 +1,9 @@
 package com.example.katecatlin.diversityapp.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.webkit.WebView;
 
 import com.example.katecatlin.diversityapp.R;
@@ -16,8 +18,13 @@ public class StatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        WebView webview = new WebView(this);
-        setContentView(webview);
-        webview.loadUrl("http://slashdot.org/");
+        Intent intent = getIntent();
+        String url = intent.getStringExtra("FULL_URL_KEY");
+        Log.d("TAG", url);
+
+
+        setContentView(R.layout.activity_stat);
+        WebView webView = (WebView) findViewById(R.id.webview);
+        webView.loadUrl(url);
     }
 }
