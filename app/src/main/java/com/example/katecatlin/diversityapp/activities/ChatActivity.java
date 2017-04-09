3267f1f74ee4ca729789b939be61e74724f806b9
Stuart Kent <skent.dev@gmail.com>
2017-04-09 11:24:08 -0600

    Handle binary question type

--- app/src/main/java/com/example/katecatlin/diversityapp/activities/ChatActivity.java
+++ app/src/main/java/com/example/katecatlin/diversityapp/activities/ChatActivity.java
@@ -14,6 +14,7 @@ import com.google.gson.Gson;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
@@ -29,6 +30,8 @@ import it.slyce.messaging.message.TextMessage;
  */
 
 public class ChatActivity extends AppCompatActivity implements UserSendsMessageListener, OnOptionSelectedListener {
+
+    public static final List<String> BINARY_QUESTION_CHOICES = Arrays.asList("Yes", "No");
 
     private Datum currentQuestion;
 
@@ -57,39 +60,40 @@ public class ChatActivity extends AppCompatActivity implements UserSendsMessageL
 
     private void askCurrentQuestion() {
         String questionType = currentQuestion.getResponse().getType();
-        if (questionType.equals("user-entry")) {
-            final TextMessage currentMessage = new TextMessage();
-            currentMessage.setText(currentQuestion.getPrompt());
-            currentMessage.setAvatarUrl("https://cdn.dribbble.com/users/28681/screenshots/2810499/robotheadshot01-dribbble_1x.jpg");
-            currentMessage.setSource(MessageSource.EXTERNAL_USER);
-            currentMessage.setDate(new Date().getTime());
-            messagingFragment.addNewMessage(currentMessage);
-        } else if (questionType.equals("binary")) {
-            final TextMessage currentMessage = new TextMessage();
-            currentMessage.setText(currentQuestion.getPrompt());
-            currentMessage.setAvatarUrl("https://cdn.dribbble.com/users/28681/screenshots/2810499/robotheadshot01-dribbble_1x.jpg");
-            currentMessage.setSource(MessageSource.EXTERNAL_USER);
-            currentMessage.setDate(new Date().getTime());
-            messagingFragment.addNewMessage(currentMessage);
-        } else if (questionType.equals("choice")) {
-            final TextMessage questionMessage = new TextMessage();
-            questionMessage.setText(currentQuestion.getPrompt());
-            questionMessage.setAvatarUrl("https://cdn.dribbble.com/users/28681/screenshots/2810499/robotheadshot01-dribbble_1x.jpg");
-            questionMessage.setSource(MessageSource.EXTERNAL_USER);
-            questionMessage.setDate(new Date().getTime());
-            messagingFragment.addNewMessage(questionMessage);
-
-            final GeneralOptionsMessage currentMessage = new VerticalGeneralOptionsMessage();
-            currentMessage.setAvatarUrl("https://cdn.dribbble.com/users/28681/screenshots/2810499/robotheadshot01-dribbble_1x.jpg");
-            currentMessage.setSource(MessageSource.EXTERNAL_USER);
-            currentMessage.setDate(new Date().getTime());
-            currentMessage.setOnOptionSelectedListener(this);
-            List<String> listOfChoices = currentQuestion.getResponse().getChoices();
-            currentMessage.setOptions(listOfChoices.toArray(new String[listOfChoices.size()]));
-
-            messagingFragment.addNewMessage(currentMessage);
+        switch (questionType) {
+            case "user-entry":
+                final TextMessage currentMessage = new TextMessage();
+                currentMessage.setText(currentQuestion.getPrompt());
+                currentMessage.setAvatarUrl("https://cdn.dribbble.com/users/28681/screenshots/2810499/robotheadshot01-dribbble_1x.jpg");
+                currentMessage.setSource(MessageSource.EXTERNAL_USER);
+                currentMessage.setDate(new Date().getTime());
+                messagingFragment.addNewMessage(currentMessage);
+                break;
+            case "binary":
+                presentChoiceMessage(currentQuestion.getPrompt(), Arrays.asList("Yes", "No"));
+                break;
+            case "choice":
+                presentChoiceMessage(currentQuestion.getPrompt(), currentQuestion.getResponse().getChoices());
+                break;
         }
+    }
 
+    private void presentChoiceMessage(String title, List<String> options) {
+        final TextMessage questionMessage = new TextMessage();
+        questionMessage.setText(title);
+        questionMessage.setAvatarUrl("https://cdn.dribbble.com/users/28681/screenshots/2810499/robotheadshot01-dribbble_1x.jpg");
+        questionMessage.setSource(MessageSource.EXTERNAL_USER);
+        questionMessage.setDate(new Date().getTime());
+        messagingFragment.addNewMessage(questionMessage);
+
+        final GeneralOptionsMessage currentMessage = new VerticalGeneralOptionsMessage();
+        currentMessage.setAvatarUrl("https://cdn.dribbble.com/users/28681/screenshots/2810499/robotheadshot01-dribbble_1x.jpg");
+        currentMessage.setSource(MessageSource.EXTERNAL_USER);
+        currentMessage.setDate(new Date().getTime());
+        currentMessage.setOnOptionSelectedListener(this);
+        currentMessage.setOptions(options.toArray(new String[options.size()]));
+
+        messagingFragment.addNewMessage(currentMessage);
     }
 
     private void updateCurrentQuestion() {
@@ -126,7 +130,18 @@ public class ChatActivity extends AppCompatActivity implements UserSendsMessageL
 
     @Override
     public String onOptionSelected(final int optionSelected) {
-        String response = currentQuestion.getResponse().getChoices().get(optionSelected);
+        String response;
+
+        switch (currentQuestion.getResponse().getType()) {
+            case "binary":
+                response = BINARY_QUESTION_CHOICES.get(optionSelected);
+                break;
+            case "choice":
+                response = currentQuestion.getResponse().getChoices().get(optionSelected);
+                break;
+            default:
+                throw new IllegalStateException("Unhandled question type");
+        }
 
         final TextMessage questionMessage = new TextMessage();
         questionMessage.setText(response);
