82c4eff94cd95f7afde03d6504805dc3e6f5c69f
Kate Catlin <kcatlin2@gmail.com>
2017-04-23 14:45:18 -0600

    Resetting testing setup

---
+++ app/src/test/java/com/example/katecatlin/ChatActivityTest.java
@@ -0,0 +1,24 @@
+package com.example.katecatlin;
+
+import com.example.katecatlin.diversityapp.activities.ChatActivity;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by katecatlin on 4/23/17.
+ */
+
+public final class ChatActivityTest {
+    private ChatActivity chatActivity;
+
+    @Before
+    public void setUp() {
+
+
+    }
+
+}
